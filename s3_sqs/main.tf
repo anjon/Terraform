46b757037ef101ee89702ccd5d3e38dc79f8ebfb
Anjon Sarker <anjon.sarker@gmail.com>
2023-04-21 20:46:21 +0200

    added sqs queue

--- s3_sqs/main.tf
+++ s3_sqs/main.tf
@@ -13,3 +13,20 @@ resource "aws_s3_bucket_acl" "upload_acl" {
   acl    = "private"
 }
 
+// Creating the SQS Queue
+resource "aws_sqs_queue" "upload_queue" {
+  name                      = "my_upload_queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
+    maxReceiveCount     = 4
+  })
+
+  tags = {
+    Environment = "production"
+  }
+}
+
