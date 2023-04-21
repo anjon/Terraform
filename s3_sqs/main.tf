6a4aef46ca0b7e8f460e063b5bd1ea65883eeab5
Anjon Sarker <anjon.sarker@gmail.com>
2023-04-21 20:37:32 +0200

    update .gitignore files

--- s3_sqs/main.tf
+++ s3_sqs/main.tf
@@ -10,6 +10,6 @@ resource "aws_s3_bucket" "upload_queue" {
 // Adding S3 Bucket ACL Policy
 resource "aws_s3_bucket_acl" "upload_acl" {
   bucket = aws_s3_bucket.upload_queue.id
-  acl = "private"
+  acl    = "private"
 }
 
