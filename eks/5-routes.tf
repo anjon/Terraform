b1cc0dda4b3b852c626fdf4c27f69fec973553f9
Anjon Sarker <anjon.sarker@gmail.com>
2023-05-03 23:47:19 +0200

    Fix CIDR Block for ipv6

--- eks/5-routes.tf
+++ eks/5-routes.tf
@@ -12,13 +12,13 @@ resource "aws_route_table" "eks_private_rt" {
       egress_only_gateway_id     = ""
       gateway_id                 = ""
       instance_id                = ""
-      ipv6_cidr_block            = ""
+      ipv6_cidr_block            = "::/0"
       local_gateway_id           = ""
       network_interface_id       = ""
       transit_gateway_id         = ""
       vpc_endpoint_id            = ""
       vpc_peering_connection_id  = ""
-    }
+    },
   ]
 
   tags = {
@@ -39,14 +39,14 @@ resource "aws_route_table" "eks_public_rt" {
       destination_prefix_list_id = ""
       egress_only_gateway_id     = ""
       instance_id                = ""
-      ipv6_cidr_block            = ""
+      ipv6_cidr_block            = "::/0"
       local_gateway_id           = ""
       nat_gateway_id             = ""
       network_interface_id       = ""
       transit_gateway_id         = ""
       vpc_endpoint_id            = ""
       vpc_peering_connection_id  = ""
-    }
+    },
   ]
 
   tags = {
