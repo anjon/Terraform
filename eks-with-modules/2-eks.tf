9679fc690d96fb1ed9a25d659e7ed73421d2e63c
Anjon Sarker <anjon.sarker@gmail.com>
2023-05-12 17:43:25 +0200

    eks with module

---
+++ eks-with-modules/2-eks.tf
@@ -0,0 +1,56 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "my-eks"
+  cluster_version = "1.25"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    disk_size = 20
+  }
+
+  eks_managed_node_groups = {
+    generla = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 5
+
+      labels = {
+        role = "general"
+      }
+
+      instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
+
+    spot = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 5
+
+      labels = {
+        role = "spot"
+      }
+
+      taints = [{
+        key    = "market"
+        value  = "spot"
+        effect = "NO_SCHEDULE"
+      }]
+
+      instance_types = ["t3.small"]
+      capacity_type  = "SPOT"
+    }
+  }
+  tags = {
+    Environment = "staging"
+  }
+}
