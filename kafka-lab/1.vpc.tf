b837ab5466a4ad44a1a9d305b589e0c3768ae359
Anjon Sarker <anjon.sarker@gmail.com>
2023-08-30 10:29:22 +0200

    This is the update of the config

--- kafka-lab/1.vpc.tf
+++
@@ -1,8 +0,0 @@
-// Creating vpc for the eks cluster
-resource "aws_vpc" "kafka_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "kafka-vpc"
-  }
-}
--- kafka-lab/2.igw.tf
+++
@@ -1,8 +0,0 @@
-// Creating internet-gateway for eks-vpc
-resource "aws_internet_gateway" "kafka_igw" {
-  vpc_id = aws_vpc.kafka_vpc.id
-
-  tags = {
-    Name = "kafka-igw"
-  }
-}
